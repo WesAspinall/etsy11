294aa9a5e291a6a3c10814a57f2efa061f5a53bf
Wesley Aspinall <wra427@gmail.com>
2015-10-07 18:14:12 -0400

    added title, shop_name, price, currency_code and img

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 (function () {
 
-  console.log('It Works!');
+ var templateString = $('#itemTemplate').text();
+ var template = _.template(templateString);
+
+ _.each(etsy.results, function(item){ 
+  console.log(item);
+  var itemHTML = template(item);
+  $('.firstHeader').append(itemHTML);
+ });
+
+
+
 
 }());
